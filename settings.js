3b67fe5e2bc700806da013330995a7a18cd37cca
JoLa-Kim <vergottinijolene37@gmail.com>
2025-05-12 19:41:24 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'JoLa', // Your name
+  botOwner: '🐥', // Your name
   ownerNumber: '27649057884', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
